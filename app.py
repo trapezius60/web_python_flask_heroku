9e1736f57b70fc279d1a060462f7784144981ed6
trapezius60 <34376406+trapezius60@users.noreply.github.com>
2021-07-25 13:07:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 from flask import Flask, request, abort, jsonify, render_template, url_for, redirect
-from linebot.models import *
-from linebot import *
+
 import json
 import requests
 
 #get other .py files
-from getIP import * #from file getIP.py
+# from getIP import * #from file getIP.py
 
 
 app = Flask(__name__)
 
 #run other .py files on this coding page
-app.register_blueprint(getIP) #ให้ file getIP.py เชื่อมกับไฟล์นี้ ในตอนที่ runserver ได้
+# app.register_blueprint(getIP) #ให้ file getIP.py เชื่อมกับไฟล์นี้ ในตอนที่ runserver ได้
 
 
 data = [{
@@ -49,16 +48,6 @@ def about():
 @app.route('/api', methods=['GET'])
 def get_api():
     return jsonify(data)
-
-line_bot_api = LineBotApi('t8TS42nUWRlHempLf4OLMEf1xoNm96YHojEt71MgX96NGuA9qucXNT/4nJtBscYdXZt/ADJLVbqfcwIbdSrlqsW0s0z6i8GPPWtipaaGnOoj0UhNrGI7eeOXAzRf4A6s1hdq+CraBNPxexpYI3TwowdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('7f819199fc35d2461ceb0191d0fb304d')
-
-
-@app.route("/callback", methods=['POST'])
-def callback():
-    body = request.get_data(as_text=True)
-    print(body)
-    return 'OK'
 
 
 if __name__ == "__main__":
