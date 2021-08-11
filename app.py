8ec8518325c3b37cb7acaf177b1618281748161a
trapezius60 <34376406+trapezius60@users.noreply.github.com>
2021-08-11 12:01:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,13 @@
-from flask import Flask, request, abort, jsonify, render_template, url_for, redirect
-
-import json
-import requests
-
-#get other .py files
-# from getIP import * #from file getIP.py
-
-
+from flask import Flask,render_template
 app = Flask(__name__)
-
-#run other .py files on this coding page
-# app.register_blueprint(getIP) #ให้ file getIP.py เชื่อมกับไฟล์นี้ ในตอนที่ runserver ได้
-
-
-
-@app.route('/')
-def hello():
-    return "Hello Flask-Heroku Github Python"
-            
-#@app.route('/')
-#def main():
- #   return render_template('index.html')
+@app.route("/")#default page of web
+def index():
+    return render_template('index.html') #อย่างลือมเปลี่ยนเป็น index.html ********************
 
 @app.route("/about")
 def about():
     return render_template('about.html')
 
-@app.route('/api', methods=['GET'])
-def get_api():
-    return jsonify(data)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
