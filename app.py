471c0a67360678cd294a6625b2586037d2d69294
trapezius60 <34376406+trapezius60@users.noreply.github.com>
2021-08-11 11:36:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,39 +7,20 @@ import requests
 # from getIP import * #from file getIP.py
 
 
-app = Flask(__name__, template_folder='templates') )
+app = Flask(__name__, template_folder='templates')
 
 #run other .py files on this coding page
 # app.register_blueprint(getIP) #ให้ file getIP.py เชื่อมกับไฟล์นี้ ในตอนที่ runserver ได้
 
 
-data = [{
-            "update":"21-07-2021"
-        },
-        {
-            "id": 1,
-            "activity": "autopsy",
-            "case": "3"
-        },
-        {
-            "id": 2,
-            "activity": "cfm",
-            "case": "5"
-        },
-        {
-            "id": 3,
-            "activity": "scene",
-            "case": "1"
-        }
-    ]
 
-#@app.route('/')
-#def hello():
- #   return "Hello Flask-Heroku Github Python"
-            
 @app.route('/')
-def main():
-    return render_template('index.html')
+def hello():
+    return "Hello Flask-Heroku Github Python"
+            
+#@app.route('/')
+#def main():
+ #   return render_template('index.html')
 
 @app.route("/about")
 def about():
